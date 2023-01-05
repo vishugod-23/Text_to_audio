031424bd18cf143ede3c0900f8c8616ed5a648e4
vishal <23vishal.02dogra@gmail.com>
2023-01-05 18:00:23 +0530

    Initial commit

---
+++ audio.py
@@ -0,0 +1,15 @@
+from playsound import playsound
+from gtts import gTTS
+
+
+def playaudio(audio):
+    playsound(audio)
+
+
+def convert_to_audio(text):
+    audio = gTTS(text)
+    audio.save("textaudio.mp3")
+    playaudio("textaudio.mp3")
+
+
+convert_to_audio("Hello Welcome ")
